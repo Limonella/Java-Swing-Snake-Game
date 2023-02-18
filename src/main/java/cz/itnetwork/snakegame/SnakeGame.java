bf51e69db75c14de0e03a38036652b308afe9103
Limonella <christov.rosen@gmail.com>
2023-02-18 11:23:38 +0100

    Snake Game

---
+++ src/main/java/cz/itnetwork/snakegame/SnakeGame.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cz.itnetwork.snakegame;
+
+/**
+ *
+ * @author chris
+ */
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        
+        new GameFrame();
+        
+    }
+}
